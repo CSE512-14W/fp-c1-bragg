66c61cec66a8226eb4ba4f446e397f8861861023
Christopher Lin <chrislin@cs.washington.edu>
2014-03-11 15:03:38 -0700

    multiedges

--- datagenerators/run.py
+++ datagenerators/run.py
@@ -3,7 +3,8 @@ from VI import *
 import json
 
 
-def outputToJSON(mdp, state, action, prob, v, policy, depth, isState):
+def outputToJSON(mdp, state, action, prob, v, 
+                 policy, depth, isState, pointOfOrigin):
     json = {}
     if isState:
         json['name'] = mdp.getStateString(state)
@@ -13,20 +14,32 @@ def outputToJSON(mdp, state, action, prob, v, policy, depth, isState):
             actions = []
             for a in mdp.getActions(state):
                 actions.append(outputToJSON(mdp, state, a, 0, 
-                                            v, policy, depth, False))
+                                            v, policy, depth, False, 0))
             json['children'] = actions
         json['type'] = "state"
+        json['poo'] = "%f" % pointOfOrigin
     else:
         json['name'] = str(action)
         json['value'] = "0"
         
         states = []
         qvalue = 0.0
+
+        lastPointOfOrigin = -0.5
+
         for (s, p) in mdp.getT(state, action):
+            #compute necessary translation for links so they all line up nicely
+            nextPointOfOrigin = lastPointOfOrigin + p / 2
+            lastPointOfOrigin += p
+
             states.append(outputToJSON(mdp, s, None, p, 
-                                       v, policy, depth-1, True))
+                                       v, policy, depth-1, True,
+                                       nextPointOfOrigin))
             qvalue += p * v[s]
+
         json['qvalue'] = qvalue
+
+        
         json['children'] = states
         json['type'] = "action"
     
@@ -41,9 +54,9 @@ print policy
 f = open('../data/gridworlddepth5.json', 'w')
 
 print outputToJSON(gridworld, gridworld.getInitialState(), None, 1.0, 
-             v, policy, 2, True)
+             v, policy, 2, True, 0)
 
 f.write(json.dumps(outputToJSON(gridworld, gridworld.getInitialState(), 
-                                None, 1.0, v, policy, 5, True),
+                                None, 1.0, v, policy, 5, True, 0),
                    indent=4))
 
